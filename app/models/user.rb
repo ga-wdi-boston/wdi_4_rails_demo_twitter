e1cb3dc134ae462f8260385fff307752e2968672
David Fisher <tibbon+github@gmail.com>
2013-07-15 11:10:57 -0400

    All models and migrations done. I think

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
 	has_many :tweets
 	has_many :favorites
-	# Do following here in a minute
+	
+	has_many :followings
+	has_many :friends, through: :followings, source: 'followed'
+
 	attr_accessible :name, :screenname, :description, :email,
 					:location, :password
 end
